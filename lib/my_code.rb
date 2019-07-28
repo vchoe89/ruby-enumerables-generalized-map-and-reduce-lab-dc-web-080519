e0f32851cd6ace75fdf7266648f8e2b40cdbea9b
Victor Jian Choe <vchoe89@gmail.com>
2019-07-28 21:30:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,12 @@
 def map(array, block)
   new_array = []
-  i = 0
+  if block
+    total = block
+    i = 0
+  else
+    total = array[0]
+  end
+  
   while i < array.length do
     block = yield(block, array[i])
     i += 1
