284d00d58c79e05ac762ff99483b87fb1d0e0be8
Victor Jian Choe <vchoe89@gmail.com>
2019-07-28 21:37:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,22 +11,6 @@ end
 
 
 
-def reduce (array, start = nil)
-  if start
-   total = start
-    i = 0
 
-  else 
-    total = array[0]
-    i = 1
-    
-  end
-  
-  while i < array.length do
-  total = yield(total, array[i])
-  i += 1
- 	end
- 	return total
-end
 
 
